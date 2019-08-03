9924741ea6009399cbb812d289c913f1042f8431
Your Name <nktclt@gmail.com>
2019-08-03 10:57:05 +0530

    program seven

--- problem_solving/staircase.py
+++ problem_solving/staircase.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/bin/python
 
 import math
 import os
@@ -8,9 +8,19 @@ import sys
 
 # Complete the staircase function below.
 def staircase(n):
+    o=0
+    p=' '
+    z=n
+    for i in range(n):
+        o=o+1
+        z=z-1
+        print(p*z+'#'*o)
+
+
+
+
 
 if __name__ == '__main__':
-    n = int(input())
+    n = int(raw_input())
 
     staircase(n)
-
