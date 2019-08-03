e2e5ee965953b73779f7a1b95f096f6377c37c76
Your Name <nktclt@gmail.com>
2019-08-03 10:50:28 +0530

    program eight

---
+++ problem_solving/min_max_sum.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+arr = list(map(int, input().strip().split(' ')))
+max=-sys.maxsize-1
+min=sys.maxsize
+sum=0
+for x in arr:
+    sum+=x
+    if x>max:
+        max=x
+    if x<min:
+        min=x
+print (sum-max,sum-min)
+
