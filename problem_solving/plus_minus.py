8abde62b204ff5bade2df134a05766250eedbf55
Your Name <nktclt@gmail.com>
2019-08-03 10:56:26 +0530

    program six

--- problem_solving/plus_minus.py
+++ problem_solving/plus_minus.py
@@ -6,8 +6,32 @@ import random
 import re
 import sys
 
+
 # Complete the plusMinus function below.
 def plusMinus(arr):
+    a=len(arr)
+    z=0
+    n=0
+    p=0
+    for i in range(a):
+        if arr[i]>0:
+            p=p+1
+
+        if arr[i]<0:
+            n=n+1
+
+        if arr[i]==0:
+            z=z+1
+    print(p/a)
+    print(n/a)
+    print(z/a)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     n = int(input())
@@ -15,4 +39,3 @@ if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
     plusMinus(arr)
-
